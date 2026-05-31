f924b0dd4906d34109273de1dddce3b5e0f031b7
Adriel Dinelli <adriel.dinelli@gmail.com>
2026-05-30 21:09:21 -0300

    Adds units selection

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,37 +4,39 @@ import axios from 'axios';
 
 window.onload = () => {
   const submitButton = document.getElementById('submit') as HTMLButtonElement;
-  const inputField = document.getElementById('address') as HTMLInputElement;
+  const addressField = document.getElementById('address') as HTMLInputElement;
+  const unitsField = document.getElementById('units') as HTMLInputElement;
   const results = document.getElementById('results') as HTMLDivElement;
   const message = document.getElementById('message') as HTMLDivElement;
 
   submitButton.addEventListener('click', async () => {
-    const address = encodeURIComponent(inputField.value)
+    const address = `address=${encodeURIComponent(addressField.value)}`;
+    const units = `units_system=${encodeURIComponent(unitsField.value)}`;
 
     try {
-    const result = await axios.get(`/api/v1/weather_forecast?address=${address}`);
+      const result = await axios.get(`/api/v1/weather_forecast?${address}&${units}`);
 
-    const d = result.data;
+      const d = result.data;
 
-    results.innerHTML = `
-      <h3>Forecast for: ${d.address}</h23>
-      <p>Today is: ${d.current_date}</p>
-      <p>Temperature: ${d.current_temperature} ${d.temperature_unit}</p>
-      <hr>
-      <h4>Next ${d.next_days.length} days forecast:</h4>
-      ${d.next_days.map((day: any) => `
-        <div>
-          <p>${day.date} - Max: ${day.maxTemperature} | Min: ${day.minTemperature}</p>
-        </div>
-      `).join('')}
-      <hr>
-      ${ d.cached ? `
-          <span>Cached result</span>
-          </br>
-          <span>Expires at: ${d.expires_at}</span>
-        ` : ''
-      }
-    `;
+      results.innerHTML = `
+        <h3>Forecast for: ${d.address}</h23>
+        <p>Today is: ${d.current_date}</p>
+        <p>Temperature: ${d.current_temperature} ${d.temperature_unit}</p>
+        <hr>
+        <h4>Next ${d.next_days.length} days forecast:</h4>
+        ${d.next_days.map((day: any) => `
+          <div>
+            <p>${day.date} - Max: ${day.maxTemperature} | Min: ${day.minTemperature}</p>
+          </div>
+        `).join('')}
+        <hr>
+        ${ d.cached ? `
+            <span>Cached result</span>
+            </br>
+            <span>Expires at: ${d.expires_at}</span>
+          ` : ''
+        }
+      `;
     } catch (error) {
       console.error(error.response);
       message.innerHTML = `
